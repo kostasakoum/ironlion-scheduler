cb335e7d70af8fd7523aaf0b29946dc795f9b1ce
kostasakoum <kostas.akoum@gmail.com>
2026-04-21 00:14:18 -0400

    Update assessments.js

--- ironlion-deploy/api/assessments.js
+++ ironlion-deploy/api/assessments.js
@@ -1,35 +1,10 @@
-const crypto = require("crypto");
-
 module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-
-  const privateKeyB64 = process.env.VITE_GOOGLE_PRIVATE_KEY || "";
-  const privateKey = Buffer.from(privateKeyB64, "base64").toString("utf8");
-
-  // Debug: show key info without exposing the key
-  const keyInfo = {
+  const privateKeyB64 = process.env.VITE_GOOGLE_PRIVATE_KEY || "not set";
+  res.status(200).json({ 
+    message: "hello",
     nodeVersion: process.version,
-    b64Length: privateKeyB64.length,
-    decodedLength: privateKey.length,
-    hasHeader: privateKey.includes("-----BEGIN PRIVATE KEY-----"),
-    hasFooter: privateKey.includes("-----END PRIVATE KEY-----"),
-    firstLine: privateKey.split("\n")[0],
-    lastLine: privateKey.split("\n").filter(l => l.trim()).pop(),
-    newlineCount: (privateKey.match(/\n/g) || []).length,
-    hasLiteralBackslashN: privateKey.includes("\\n"),
-  };
-
-  // Try to create a key object
-  try {
-    const keyObj = crypto.createPrivateKey({
-      key: privateKey,
-      format: "pem",
-    });
-    keyInfo.keyObjectType = keyObj.type;
-    keyInfo.keyObjectAsymmetricType = keyObj.asymmetricKeyType;
-  } catch (e) {
-    keyInfo.keyObjectError = e.message;
-  }
-
-  res.status(200).json(keyInfo);
+    keyB64Start: privateKeyB64.substring(0, 20),
+    keyB64Length: privateKeyB64.length
+  });
 };
